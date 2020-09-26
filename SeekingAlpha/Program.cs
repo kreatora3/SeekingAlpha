42830f6029be56fed94a50433bd124065880a1aa
Ivan Tochev <annihilatora@gmail.com>
2020-09-26 14:52:35 +0300

    s

--- SeekingAlpha/Program.cs
+++ SeekingAlpha/Program.cs
@@ -10,7 +10,7 @@ namespace SeekingAlpha
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world");
+            Console.WriteLine("hello world ");
         }
     }
 }
